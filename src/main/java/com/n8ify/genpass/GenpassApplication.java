4b39c7ed6249c3418ef156d1ee92002fa96dc1b5
mfec <mfec@mfecs-Air.domain.name>
2018-08-05 12:10:52 +0700

    Initialize project

---
+++ src/main/java/com/n8ify/genpass/GenpassApplication.java
@@ -0,0 +1,12 @@
+package com.n8ify.genpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenpassApplication.class, args);
+	}
+}
---
+++ src/test/java/com/n8ify/genpass/GenpassApplicationTests.java
@@ -0,0 +1,16 @@
+package com.n8ify.genpass;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GenpassApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
